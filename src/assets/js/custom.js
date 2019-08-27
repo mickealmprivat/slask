2a537b06526b57a719dcb29b981695f59de5eccd
Mikael Alm <mikalm@TECH-MIKALM-4.local>
2019-08-27 19:54:40 +0200

    Fixed the fixed header

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
     var innerDistance = $('.portfolio-case__logo').position().top / 2;
     var body = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-		offset: -innerDistance
-    })
-    .triggerElement(".portfolio-case__logo")
+    new ScrollMagic.Scene()
+    .triggerElement(".portfolio-case__header--min-top")
     .triggerHook("onLeave")
     .setClassToggle('.portfolio-case__header--min', 'is-visible')
+    .on('start', function() {
+      $('.portfolio-case__header--min h1, .portfolio-case__header--min svg').slideToggle(150);
+    })
 	.addTo(body); 
 });
 
