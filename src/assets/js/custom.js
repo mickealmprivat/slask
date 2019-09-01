79b998736769c42b4d3430b421f268a9ee91188f
Micke Alm <mickealmcrap@gmail.com>
2019-09-01 09:44:45 +0200

    Working on LL

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,15 +1,36 @@
 "use strict";
 $(document).ready(function() {
-    var innerDistance = $('.portfolio-case__logo').position().top / 2;
-    var body = new ScrollMagic.Controller();
+  // Navigation 
+  var innerDistance = $('.portfolio-case__logo').position().top / 2;
+  var controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene()
+  new ScrollMagic.Scene()
     .triggerElement(".portfolio-case__header--min-top")
     .triggerHook("onLeave")
     .setClassToggle('.portfolio-case__header--min', 'is-visible')
     .on('start', function() {
       $('.portfolio-case__header--min h1, .portfolio-case__header--min svg').slideToggle(150);
     })
-	.addTo(body); 
+  .addTo(controller); 
+  
+  
+  // Video
+  $('video').each(function(t) {
+    t = $(this);
+    console.log(t[0]);
+
+    new ScrollMagic.Scene()
+    .triggerElement($('video'))
+    .triggerHook("onEnter")
+    .setClassToggle('video-is-loaded')
+    .on("enter", function(elem) {
+      console.log(elem);
+      // $(self.children).each(function() {
+      //   var src = self.attr('data-src');
+      //   console.log(self);
+      // })
+    })
+    .addTo(controller)
+  })
 });
 
